835d5eb1ac876cfa3fae4032c8666073c95f06e3
David Cabrera <davidch@ugr.es>
2022-11-09 23:39:42 +0100

    Create User class

---
+++ User.js
@@ -0,0 +1,12 @@
+class User {
+    constructor(id, email, password) {
+        
+        this.id = id
+        this.email = email;
+        this.password = password;
+        this.comments = []
+
+    }
+}
+
+// console.log(new User("davidch@ugr.es", "PASSWORD").password);
