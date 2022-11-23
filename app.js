7e0a5ff431c6220aac2ea297de2a4bea7caff5d3
David Cabrera <davidch@ugr.es>
2022-11-23 22:25:36 +0100

    add app.js

---
+++ app.js
@@ -0,0 +1,56 @@
+// npm run dev
+const express = require('express');
+const DataBaseMock = require('./DataBaseMock');
+const storage = require('node-sessionstorage')
+ 
+const app = express();
+DB = new DataBaseMock()
+
+app.get('/', (req, res) => {
+
+    res.send('Hello world')
+    res.end()
+
+})
+
+app.get('/BoardGame', (req, res) => {
+
+    res.send(JSON.stringify(DB.GetBoardGames()))
+    res.end()
+
+})
+
+app.post('/BoardGame', (req, res) => {
+
+    const { Email, Password } = req.body;
+    
+    /////////////////////////////////////////////////////////////////////////////
+    // check if is correct, then save it
+    storage.setItem('Email', Email)
+    storage.setItem('Password', Password)
+    
+    res.send({
+        Username,
+        Password,
+        NewBoardGame
+        // authorization,
+    });
+
+})
+
+app.post('/Login', (req, res) => {
+
+    const { Username, Password } = req.body;
+    // const { authorization } = req.headers;
+    res.send({
+        Username,
+        Password,
+        NewBoardGame
+        // authorization,
+    });
+
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,console.log(`http://localhost:${PORT}`));
